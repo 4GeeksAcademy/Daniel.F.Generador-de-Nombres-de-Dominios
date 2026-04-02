808320c0a8c24a6f230dad1965613696e003a5c0
Daniel Fabelo <daniel-fabelo-santana@outlook.es>
2026-04-02 09:50:40 +0000

    Primer commit

--- src/app.js
+++ src/app.js
@@ -5,7 +5,17 @@ import "./style.css";
 import "./assets/img/rigo-baby.jpg";
 import "./assets/img/4geeks.ico";
 
-window.onload = function() {
-  //write your code here
-  console.log("Hello Rigo from the console!");
-};
+
+let pronoun = ['the', 'your', 'her', 'his', 'my'];
+let adj = ['fantastic', 'amazing', 'smaler', 'coolest'];
+let noun = ['dog', 'cat', 'bird', 'mecha', 'beyblade', 'pokemon'];
+
+window.onload = function () {
+  for (let i = 0; i < pronoun.length; i++) {
+    for (let j = 0; j < adj.length; j++) {
+      for (let k = 0; k < noun.length; k++) {
+        console.log(pronoun[i] + adj[j] + noun[k] + '.com');
+      }
+    }
+  }
+}
